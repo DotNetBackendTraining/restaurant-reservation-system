8a2938ec166c34588e855fea66f59bcb8f7e61c8
Izzat Alsharif <work@izzatalsharif.com>
2024-05-04 16:56:49 +0300

    Add documentation

--- RestaurantReservation.Domain/Interfaces/Repositories/IEmployeeRepository.cs
+++ RestaurantReservation.Domain/Interfaces/Repositories/IEmployeeRepository.cs
@@ -5,7 +5,12 @@ namespace RestaurantReservation.Domain.Interfaces.Repositories;
 public interface IEmployeeRepository
 {
     IAsyncEnumerable<Employee> GetAllManagersAsync();
+
     Task<EmployeeRestaurantDetail?> GetEmployeeRestaurantDetailAsync(int employeeId);
+
+    /// <exception cref="InvalidOperationException">If no employee with the given ID was found</exception>
     Task<double> CalculateAverageOrderAmountAsync(int employeeId);
+
+    /// <exception cref="InvalidOperationException">If no restaurant with the given ID was found</exception>
     Task<decimal> GetTotalRevenueByRestaurant(int restaurantId);
 }
