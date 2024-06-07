1817a9093cbe679f7bcd252760f8717917fc5fe5
Izzat Alsharif <work@izzatalsharif.com>
2024-06-07 16:18:52 +0300

    Remove unused interfaces

--- RestaurantReservation/Presentation/Interfaces/ICudInterface.cs
+++
@@ -1,8 +0,0 @@
-namespace RestaurantReservation.Presentation.Interfaces;
-
-public interface ICudInterface<in TEntity>
-{
-    Task Create(TEntity entity);
-    Task Update(TEntity entity);
-    Task Delete(TEntity entity);
-}
