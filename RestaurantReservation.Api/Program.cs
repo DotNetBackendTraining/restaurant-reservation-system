39c8f36e357adca1ac4c1ef5a2a70550b33d1389
Izzat Alsharif <work@izzatalsharif.com>
2024-06-14 20:18:25 +0300

    Add Swagger documentation

--- RestaurantReservation.Api/Program.cs
+++ RestaurantReservation.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using RestaurantReservation.Api.Auth.Common;
 using RestaurantReservation.App.Configuration;
 using RestaurantReservation.App.Configuration.Db;
@@ -9,29 +10,55 @@ builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("D
 
 // Add services to the container.
 builder.Services.AddApplicationServices();
-builder.Services.AddControllersWithViews()
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddControllers()
     .AddNewtonsoftJson();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
