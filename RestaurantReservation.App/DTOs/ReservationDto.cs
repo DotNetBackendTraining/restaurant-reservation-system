09facd5564ed32f87681d022b12cf8deac6f67a7
Izzat Alsharif <work@izzatalsharif.com>
2024-07-06 01:15:41 +0300

    Update ReservationDto

--- RestaurantReservation.App/DTOs/ReservationDto.cs
+++ RestaurantReservation.App/DTOs/ReservationDto.cs
@@ -2,6 +2,8 @@ namespace RestaurantReservation.App.DTOs;
 
 public record ReservationDto(
     int ReservationId,
+    int CustomerId,
+    int TableId,
     DateTime ReservationDate,
     int PartySize)
 {
