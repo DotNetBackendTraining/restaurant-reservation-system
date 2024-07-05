93e0fdb00a9d1ae558a74a8b2ca9e62b804085bf
Izzat Alsharif <work@izzatalsharif.com>
2024-07-05 15:03:52 +0300

    Add documentation

--- RestaurantReservation.App/Interfaces/Services/ICudService.cs
+++ RestaurantReservation.App/Interfaces/Services/ICudService.cs
@@ -3,7 +3,14 @@ namespace RestaurantReservation.App.Interfaces.Services;
 public interface ICudService<TDto>
     where TDto : class
 {
+    /// <summary>
+    /// Converts the dto to entity and inserts it into the database.
+    /// Returns the newly created object mapped back to dto.
+    /// </summary>
+    /// <param name="dto">DTO of the entity to be inserted</param>
+    /// <returns>DTO of the new entity</returns>
     Task<TDto> CreateAsync(TDto dto);
+
     Task UpdateAsync(TDto dto);
     Task DeleteAsync(TDto dto);
 }
